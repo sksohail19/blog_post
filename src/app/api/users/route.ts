0cfacae0aeec3f9c037ba9517ba98f5f3306b9f3
sksohail19 <shaiksohail0726@gmail.com>
2026-04-22 09:43:52 +0000

    removed routes

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(request: NextRequest) {
-    // try {
-    //     // Simulate fetching user data from a database
-    //     const users = 
-    // }
-}
